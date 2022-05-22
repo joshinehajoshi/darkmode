afd5eac4248233c9746bbe2452701fcad014c826
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-05-22 21:29:08 +0530

    button added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [mode, setMode] = useState(false);
+  const modeHandler = () => {
+    setMode(!mode);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className={mode ? "App-header" : "App-header-light"}>
+      <button onClick={modeHandler}>Dark Mode</button>
       </header>
     </div>
   );
