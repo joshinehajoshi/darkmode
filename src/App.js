915a83e9d84a5a99339dabd0232b7500f91189ee
neha-butterpaper <neha.joshi@butterpaper.tech>
2022-05-23 11:30:27 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import {Button, Navbar, Nav, Container, NavDropdown, Form, FormControl} from 'react-bootstrap';
 import './App.css';
 
 function App() {
@@ -7,10 +8,38 @@ function App() {
     setMode(!mode);
   }
   return (
-    <div className="App">
-      <header className={mode ? "App-header" : "App-header-light"}>
-      <button onClick={modeHandler}>Dark Mode</button>
-      </header>
+    <div className={mode ? "App-header" : "App-header-light"}>
+     
+
+      <Navbar bg="light" expand="lg">
+  <Container fluid>
+    <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+    <Navbar.Toggle aria-controls="navbarScroll" />
+    <Navbar.Collapse id="navbarScroll">
+      <Nav
+        className="me-auto my-2 my-lg-0"
+        style={{ maxHeight: '100px' }}
+        navbarScroll
+      >
+        <Nav.Link href="#action1">Home</Nav.Link>
+        <Nav.Link href="#action2">Link</Nav.Link>
+        <NavDropdown title="Link" id="navbarScrollingDropdown">
+          <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action5">
+            Something else here
+          </NavDropdown.Item>
+        </NavDropdown>
+        <Nav.Link href="#" disabled>
+          Link
+        </Nav.Link>
+      </Nav>
+     <Button onClick={modeHandler}>Dark Node</Button>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
+<h1>Hello</h1>
     </div>
   );
 }
